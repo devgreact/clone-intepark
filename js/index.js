4a88f5e1000fbd8ac12e55474c899e5e322f2f5b
devgreact <dev.greact@gmail.com>
2023-05-11 13:13:06 +0900

    update: UI 개선

--- js/index.js
+++ js/index.js
@@ -49,6 +49,10 @@ window.onload = function () {
       fill: "row",
     },
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".shopping .sw-next",
+      prevEl: ".shopping .sw-prev",
+    },
     breakpoints: {
       1024: {
         spaceBetween: 32,
@@ -78,6 +82,10 @@ window.onload = function () {
       fill: "row",
     },
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
     breakpoints: {
       1024: {
         spaceBetween: 32,
@@ -103,6 +111,10 @@ window.onload = function () {
   let ticketSwiper = new Swiper(".sw-ticket", {
     slidesPerView: "auto",
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".ticket .sw-next",
+      prevEl: ".ticket .sw-prev",
+    },
     breakpoints: {
       1024: {
         slidesPerView: 3,
@@ -118,6 +130,10 @@ window.onload = function () {
   let liveSwiper = new Swiper(".sw-live", {
     slidesPerView: 4,
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".live .sw-next",
+      prevEl: ".live .sw-prev",
+    },
     breakpoints: {
       1024: {
         slidesPerView: 3,
@@ -136,6 +152,10 @@ window.onload = function () {
       fill: "row",
     },
     spaceBetween: 19,
+    navigation: {
+      nextEl: ".books .sw-next",
+      prevEl: ".books .sw-prev",
+    },
     breakpoints: {
       1024: {
         slidesPerView: 3,
@@ -159,6 +179,10 @@ window.onload = function () {
   let eventsSwiper = new Swiper(".sw-events", {
     slidesPerView: 3,
     spaceBetween: 27,
+    navigation: {
+      nextEl: ".event .sw-next",
+      prevEl: ".event .sw-prev",
+    },
     breakpoints: {
       1280: {
         slidesPerView: 4,
